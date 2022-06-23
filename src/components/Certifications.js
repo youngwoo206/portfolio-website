9e6be75256fa82ff14ee0e1dbdc824873c1ebd1e
Youngwoo <youngwoo206@gmail.com>
2022-06-22 20:24:46 -0400

    started updating certifications section

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Certs from "./Certs";
+import ProjectsCard from "./ProjectsCard";
 import "../styles/Certifications.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
@@ -7,29 +8,54 @@ import mosExcel from "../assets/mosExcel.png";
 import certPlaceholder from "../assets/certPlaceholder.png";
 
 
+
 function Certifications() {
   return (
     <div className="certBanner">
-      <h1>~my certifications~</h1>
+      <h1>~projects and certifications~</h1>
 
 
       <div
         className='certBody'
->
+        style={{ display: "flex", flex: 1, flexDirection: "column" }}
+      >
+
+        <div className="projectDiv col-12 col-md-8">
+        <Carousel
+            className="projectsCarousel"
+            infiniteLoop={true}
+            showArrows={true}
+            showIndicators={true}
+            axis='vertical'
+          >
+            <div>
+              <ProjectsCard>
+                <h2>iGem Design Team</h2>
+              </ProjectsCard>
+            </div>
+
+            <div>
+              <ProjectsCard>
+                <h2>Fitness Tracker</h2>
+              </ProjectsCard>
+            </div>
 
 
-          <div className="carouselDiv" id="certs">
+        </Carousel>
+        </div>
+
+
+        <div className="carouselDiv col-12 col-md-4"
+          id="certs"
+        >
             <Carousel
-              className="carousel"
-              centerMode={true}
-              centerSlidePercentage={30}
-              infiniteLoop={true}
+            className="carousel"
+            infiniteLoop={true}
+            showArrows={true}
+            showIndicators={true}
             >
               <div>
                 <Certs thumb={mosExcel} name="MOS Excel 2016" date="03/01/2022" />
-              </div>
-              <div>
-                <Certs thumb={certPlaceholder} name="Coming" date="soon" />
               </div>
               <div>
                 <Certs thumb={certPlaceholder} name="Coming" date="soon" />
---
+++ src/components/ProjectsCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ProjectsCard(props) {
+  return (
+    <div className='projectCard'>
+        
+        {props.children}
+
+
+    </div>
+  )
+}
+
+export default ProjectsCard
